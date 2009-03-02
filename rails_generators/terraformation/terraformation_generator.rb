fd2ada347b6435a3a62a63c4e442cbea139a4514
Tim Pope <code@tpope.net>
2009-03-02 13:53:38 -0500

    script/generate terraformation --full

--- rails_generators/terraformation/terraformation_generator.rb
+++ rails_generators/terraformation/terraformation_generator.rb
@@ -1,4 +1,11 @@
+require File.dirname(__FILE__) + '/../terraforming'
+
 class TerraformationGenerator < Rails::Generator::Base
+  include Terraforming
+
+  def option?(key)
+    options.fetch(key, options[:full])
+  end
 
   def manifest
     record do |m|
@@ -6,12 +13,12 @@ class TerraformationGenerator < Rails::Generator::Base
       m.file      'clear_test_default.rake',     'lib/tasks/clear_test_default.rake'
       m.template  'application.html.haml.erb',   'app/views/layouts/application.html.haml'
       m.file      'jquery.js',                   'public/javascripts/jquery.js'
-      if options[:blueprint]
+      if option?(:blueprint)
         m.file 'blueprint/ie.css',               'public/stylesheets/blueprint/ie.css'
         m.file 'blueprint/print.css',            'public/stylesheets/blueprint/print.css'
         m.file 'blueprint/screen.css',           'public/stylesheets/blueprint/screen.css'
       end
-      if options[:gitignore]
+      if option?(:gitignore)
         m.file 'gitignore',                      '.gitignore'
         m.file 'null_gitignore',                 'tmp/.gitignore'
         m.file 'null_gitignore',                 'log/.gitignore'
@@ -29,5 +36,6 @@ class TerraformationGenerator < Rails::Generator::Base
     super
     opt.on("--[no-]blueprint", "Install Blueprint") { |v| options[:blueprint] = v }
     opt.on("--[no-]gitignore", "Default gitignore") { |v| options[:gitignore] = v }
+    opt.on("--[no-]full", "Everything")             { |v| options[:full]      = v }
   end
 end
