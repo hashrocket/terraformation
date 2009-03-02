41182a4abcd6de41f3ea24594aa0f7716f7b4627
Tim Pope <code@tpope.net>
2009-03-02 13:00:22 -0500

    Optional blueprint support

--- rails_generators/terraformation/terraformation_generator.rb
+++ rails_generators/terraformation/terraformation_generator.rb
@@ -2,9 +2,15 @@ class TerraformationGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      m.template  'application.html.haml.erb',   'app/views/layouts/application.html.haml'
+      m.directory File.join('public/stylesheets/blueprint') if options[:blueprint]
       m.file      'clear_test_default.rake',     'lib/tasks/clear_test_default.rake'
+      m.template  'application.html.haml.erb',   'app/views/layouts/application.html.haml'
       m.file      'jquery.js',                   'public/javascripts/jquery.js'
+      if options[:blueprint]
+        m.file 'blueprint/ie.css',               'public/stylesheets/blueprint/ie.css'
+        m.file 'blueprint/print.css',            'public/stylesheets/blueprint/print.css'
+        m.file 'blueprint/screen.css',           'public/stylesheets/blueprint/screen.css'
+      end
     end
   end
 
@@ -14,4 +20,8 @@ class TerraformationGenerator < Rails::Generator::Base
     "Usage: #{$0} terraformation"
   end
 
+  def add_options!(opt)
+    super
+    opt.on("--[no-]blueprint", "Install Blueprint") { |v| options[:blueprint] = v }
+  end
 end
